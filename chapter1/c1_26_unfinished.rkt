d77a769c357847ce18b192d38b4a15c1f0e534fe
liwei <liwei@jike.com>
2013-06-30 00:16:37 +0800

    26

---
+++ chapter1/c1_26_unfinished.rkt
@@ -0,0 +1,5 @@
+;; The reason seems really obvious, but I think I should provide
+;; a serious provation. Unfortunately, I don't know how to do it for
+;; this problem. 
+;; I will come back to the problem after review the introduction to
+;; algorithm.
