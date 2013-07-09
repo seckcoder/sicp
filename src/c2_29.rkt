8850fd788db9e48022fc38d9c3e987071cb77b81
liwei <liwei@jike.com>
2013-07-09 23:44:50 +0800

    c2 29 update

--- src/c2_29.rkt
+++ src/c2_29.rkt
@@ -1,37 +1,44 @@
 #lang racket
 
 (require "base.rkt")
-(define (make-mobile left right)
-  (cons left right))
+(require "mobile.rkt")
 
-(define (make-branch length structure)
-  (cons length structure))
+(define (branch-weight branch)
+  (total-weight (branch-structure branch)))
 
-(define (left-branch mobile)
-  (car mobile))
-
-(define (right-branch mobile)
-  (cdr mobile))
-
-(define (branch-length branch)
-  (car branch))
-
-(define (branch-structure branch)
-  (cdr branch))
-
-(define (leave-branch? branch)
-  (atom? (cdr branch)))
-
-(define (branch? branch)
-  (atom? (car branch)))
-
+;; (a)
 (define (total-weight mobile)
-  (display mobile)
+  (if (atom? mobile)
+    mobile
+    (+ (branch-weight (left-branch mobile))
+       (branch-weight (right-branch mobile)))))
+
+(define (branch-torque branch)
+  (* (branch-length branch)
+     (branch-weight branch)))
+
+;; (b)
+(define (balanced? mobile)
+  (if (atom? mobile)
+    true
+    (and (balanced? (branch-structure (left-branch mobile)))
+         (balanced? (branch-structure (right-branch mobile)))
+         (= (branch-torque (left-branch mobile))
+            (branch-torque (right-branch mobile))))))
+
+;; (c)
+; replace mobile1.rkt with mobile.rkt
+; and we have no need to change code in c2_29.rkt.
+; we just need to modify the left-branch/right-branch/branch-length..
+; in mobile.rkt
+
+(define (demo-mobile mobile1)
+  (display mobile1)
   (newline)
-  (if (leave-branch? mobile)
-    (branch-structure mobile)
-    (+ (total-weight (left-branch mobile))
-       (total-weight (right-branch mobile)))))
+  (display (total-weight mobile1))
+  (newline)
+  (display (balanced? mobile1))
+  (newline))
 
 (define (demo)
   (define br1 (make-branch 1 3))
@@ -40,7 +47,16 @@
                            (make-mobile br1
                                         br2)))
   (define br4 (make-branch 4 5))
-  (define mobile (make-mobile br4 br3))
-  (display mobile))
-  ;(total-weight mobile))
+  (define mobile1 (make-mobile br4 br3))
+  (define mobile2 (let* ((br1 (make-branch 4 5))
+                         (br2 (make-branch 1 8))
+                         (br3 (make-branch 4 2))
+                         (br4 (make-branch 2
+                                           (make-mobile br2
+                                                        br3))))
+                    (make-mobile br4
+                                 br1)))
+
+  (demo-mobile mobile1)
+  (demo-mobile mobile2))
 (demo)
---
+++ src/mobile1.rkt
@@ -0,0 +1,21 @@
+#lang racket
+
+(define (make-mobile left right)
+  (list left right))
+
+(define (make-branch length structure)
+  (list length structure))
+
+(define (left-branch mobile)
+  (car mobile))
+
+(define (right-branch mobile)
+  (car (cdr mobile)))
+
+(define (branch-length branch)
+  (car branch))
+
+(define (branch-structure branch)
+  (car (cdr branch)))
+
+(provide (all-defined-out))
