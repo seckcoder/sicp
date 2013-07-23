e5821d622d7bfeb7f04c6080aaf0ff5b6df56c11
seckcoder <seckcoder@Gmail.com>
2013-07-23 21:43:58 +0800

    c2 81-82

---
+++ src/c2_82.scm
@@ -0,0 +1,2 @@
+; This strategy apparently not works. For an defined operation intable:
+; op(numeric, rational, complex), the op will not be tried.
--- src/lib/base.scm
+++ src/lib/base.scm
@@ -9,6 +9,9 @@
   (define operation-table (make-table))
   (define get (operation-table 'lookup-proc))
   (define put (operation-table 'insert-proc!))
+  (define coercion-table (make-table))
+  (define get-coercion (coercion-table 'lookup-proc))
+  (define put-coercion (coercion-table 'insert-proc!))
   (define (attach-tag type-tag contents)
     (cons type-tag contents))
   (define (type-tag datum)
@@ -23,12 +26,26 @@
     (* x x))
   (define (apply-generic op . args)
     ; args corresponding to the args of op
+    (define (error-msg)
+      (error 'apply-generic "No method for these types"
+             (list op type-tags)))
+
     (let ((type-tags (map type-tag args)))
       (let ((proc (get op type-tags)))
         (if proc
           (apply proc (map contents args))
-          (error
-            'apply-generic
-            "No method for these types"
-            (list op type-tags))))))
+          (if (= (length args) 2)
+            (let ((type1 (car type-tags))
+                  (type2 (cadr type-tags)))
+              (if (eq? type1 type2)
+                (error-msg)
+                (let ((t1->t2 (get-coercion type1 type2))
+                      (t2->t1 (get-coercion type2 type1)))
+                  (cond (t1->t2
+                          (apply-generic op (t1->t2 a1) a2))
+                        (t2->t1
+                          (apply-generic op a1 (t2->t1 a2)))
+                        (else
+                          (error-msg))))))
+            (error-msg))))))
   )
