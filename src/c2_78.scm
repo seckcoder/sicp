bfba1c01022e1d16321c19cd2f256ef3952df398
seckcoder <seckcoder@Gmail.com>
2013-07-23 19:58:20 +0800

    c2_78

---
+++ src/c2_78.scm
@@ -0,0 +1,9 @@
+(import (rnrs)
+        (base)
+        (init)
+        (generic-arithmetic))
+
+(init)
+(display (make-scheme-number 3))(newline)
+(display (add (make-scheme-number 3)
+              (make-scheme-number 4)))(newline)
