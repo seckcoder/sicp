c198ee392b01f0d16d7a0fc7213e54808e2ea3ff
seckcoder <seckcoder@gmail.com>
2013-07-31 21:19:21 +0800

    constraint on-new-value

--- src/constraint.scm
+++ src/constraint.scm
@@ -1,18 +1,3 @@
-;connector:
-    ;has-value
-    ;set-new-value!
-    ;set-no-value
-    ;connect
-
-;add-constraint
-    ;on-new-value
-    ;on-no-value
-
-;mul-constraint
-    ;on-new-value
-    ;on-no-value
-
-
 (define (for-each-except excluded1 action included)
   (for-each (lambda (obj)
               (if (not (eq? obj excluded1))
@@ -23,26 +8,36 @@
   (let ((value '())
         (constraints '()))
     (define (has-value)
-      (null? value))
+      (not (null? value)))
 
     (define (set-new-value! new-value informant)
       (cond ((not (has-value))
-            (set! value new-value)
-            (for-each-except informant
-                             on-new-value
-                       constraints))
+             (set! value new-value)
+             (for-each-except informant
+                              on-new-value
+                              constraints))
             ((and (has-value)
-                  (not (= value) (new-value)))
+                  (not (= value new-value)))
              (error 'set-new-value "CONFLICT NEWVALUE" new-value))
             (else
               'ignore)
             ))
+
+    (define (set-lost-value! informant)
+      (cond ((not (has-value))
+             (error 'set-lost-value! "NO VALUE"))
+            (else
+              (set! value '())
+              (for-each-except informant
+                               on-lost-value
+                               constraints))))
 
     (define (connect constraint)
       (set! constraints (cons constraint constraints)))
 
     (define (dispatch m)
       (cond ((eq? m 'set-new-value) set-new-value!)
+            ((eq? m 'set-lost-value) set-lost-value!)
             ((eq? m 'connect) connect)
             ((eq? m 'has-value) (has-value))
             ((eq? m 'get-value) value)
@@ -63,20 +58,23 @@
 (define (set-new-value! connector new-value informant)
   ((connector 'set-new-value) new-value informant))
 
+(define (set-lost-value! connector informant)
+  ((connector 'set-lost-value) informant))
+
 (define (make-adder a1 a2 s)
   (define (on-new-value)
     (cond ((and (has-value a1)
                 (has-value a2))
            (set-new-value! s (+ (get-value a1)
-                               (get-value a2)) self))
+                                (get-value a2)) self))
           ((and (has-value a1)
                 (has-value s))
            (set-new-value! a2 (- (get-value s)
-                                (get-value a1)) self))
+                                 (get-value a1)) self))
           ((and (has-value a2)
                 (has-value s))
            (set-new-value! a1 (- (get-value s)
-                                (get-value a2)) self))
+                                 (get-value a2)) self))
           (else
             'ignore)))
   (define (self msg)
@@ -110,6 +108,7 @@
                                  (get-value m2)) self))
           (else
             'ignore)))
+
   (define (self msg)
     (cond ((eq? msg 'on-new-value) (on-new-value))
           (else
@@ -120,10 +119,64 @@
   self)
 
 (define (make-constant constant connector)
-  (set-new-value! connector constant self)
   (define (self msg)
     (error 'constant-self "UNKNOWN MESSAGE" msg))
+  (set-new-value! connector constant self)
+  (connect connector self)
+  self)
+
+(define (make-probe name connector)
+  (define (on-new-value)
+    (display name)
+    (display " Got value :")
+    (display (get-value connector))
+    (newline)
+    )
+  (define (on-lost-value)
+    (display name)
+    (display " lost value: ")
+    (newline)
+    )
+  (define (self msg)
+    (cond ((eq? msg 'on-new-value) (on-new-value))
+          ((eq? msg 'on-lost-value) (on-lost-value))
+          (else
+            (error 'probe-self "UNKNOWN MESSAGE" msg))))
+  (connect connector self)
   self)
 
 (define (on-new-value constraint)
   (constraint 'on-new-value))
+
+(define (on-lost-value constraint)
+  (constraint 'on-lost-value))
+
+(define (parse formula)
+  (cond ((null? formula) '())))
+
+
+(define (celsius-fahrenheit-converter c f)
+  (let ((u (make-connector))
+        (v (make-connector))
+        (w (make-connector))
+        (x (make-connector))
+        (y (make-connector)))
+    (make-multiplier c w u)
+    (make-multiplier v x u)
+    (make-adder v y f)
+    (make-constant 9 w)
+    (make-constant 5 x)
+    (make-constant 32 y)
+    'ok))
+
+
+(define (test-cf-converter)
+  (define C (make-connector))
+  (define F (make-connector))
+  (celsius-fahrenheit-converter C F)
+  (make-probe "Celsius temp" C)
+  (make-probe "Fahrenheit temp" F)
+  (set-new-value! C 25 'user)
+  )
+
+(test-cf-converter)
