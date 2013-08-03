7ac5d67e38f521f558fba6e2817a9b14d472f2b8
seckcoder <seckcoder@Gmail.com>
2013-08-03 13:13:07 +0800

    c3 45

---
+++ src/c3_45.scm
@@ -0,0 +1,15 @@
+(define (serialized-exchange account1 account2)
+  (let ((serializer1 (account1 'serializer))
+        (serializer2 (account2 'serializer)))
+    ((serializer1 (serializer2 exchange))
+     account1
+     account2)))
+(define (exchange account1 account2)
+  (let ((difference (- (account1 'balance)
+                       (account2 'balance))))
+    ((account1 'withdraw) difference)
+    ((account2 'deposit) difference)))
+
+
+;; This question is implementation dependent.  If use the implementation suggested
+;; by sicp, it will cause deadlock since the same procedure is serialized twice.
