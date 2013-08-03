9de61652d3502c028ef1f728aec9d212feb984cb
seckcoder <seckcoder@Gmail.com>
2013-08-03 14:07:41 +0800

    semaphore

--- src/lib/concurrency.scm
+++ src/lib/concurrency.scm
@@ -1,6 +1,10 @@
 (library
   (concurrency)
-  (export parallel-execute)
+  (export parallel-execute
+          make-semaphore
+          semaphore-acquire
+          semaphore-release)
+
   (import (chezscheme))
 
   (define (parallel-execute . args)
@@ -15,4 +19,39 @@
             (mutex 'release)
             val))
         serialized-p)))
+  (define (make-semaphore n)
+    (let ((mutex (make-mutex)))
+      (define (acquire . args)
+        (let ((block? (if (null? args)
+                        #t
+                        (car args))))
+          (cond ((= n 0)
+                 (if block?
+                   (acquire)
+                   #f))
+                ((> n 0)
+                 (mutex-acquire mutex)
+                 (set! n (- n 1))
+                 (mutex-release mutex)
+                 #t)
+                (else
+                  (error 'semaphore-acquire "n < 0")))
+          ))
+      (define (release)
+        (mutex-acquire mutex)
+        (set! n (+ n 1))
+        (mutex-release mutex))
+
+      (define (self msg)
+        (cond ((eq? msg 'acquire) acquire)
+              ((eq? msg 'release) release)
+              (else
+                (error 'semaphore-self "UNKNOWN MESSAGE" msg))))
+      self))
+
+  (define (semaphore-acquire sem . args)
+    (apply (sem 'acquire) args))
+
+  (define (semaphore-release sem)
+    (sem 'release))
   )
--- src/lib/functional.scm
+++ src/lib/functional.scm
@@ -1,11 +1,15 @@
 (library
   (functional)
   (export compose
-          compose-n)
+          compose-n
+          apply-n)
   (import (rnrs))
   (define (compose f g)
     (lambda (arg0 . rest)
       (f (apply g (cons arg0 rest)))))
+
+  ; compose a function for n times, and return a function that will make
+  ; f executed for n times if evaluated.
   (define (compose-n f n)
     (lambda args
       (define (recur k)
@@ -15,4 +19,13 @@
               (else
                 'ok)))
       (recur n)))
+
+  ; apply a function for n times with specified arg list
+  (define (apply-n f args n)
+    (cond ((= n 0) '())
+          ((> n 0)
+           (cons (apply f args)
+                 (apply-n f args (- n 1))))
+          (else
+            'ignore)))
   )
