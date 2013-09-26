f9b1b268585425663eedbef1958238bee10f8c2a
seckcoder <seckcoder@gmail.com>
2013-09-26 14:44:16 +0800

    c4 2

---
+++ src/eval/c4_2.scm
@@ -0,0 +1,4 @@
+; a) of course not. The precedence of match rule for application? is small
+; compared to assignment. So every pair will be matched by application
+;
+; b) wtf. This question is really weird.
