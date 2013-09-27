2fd4e024692c5c53cdb562cabf7f19234edcb3d1
seckcoder <seckcoder@gmail.com>
2013-09-27 20:37:07 +0800

    c4 8;

---
+++ src/eval/utils/guile.scm
@@ -0,0 +1,18 @@
+(define (assert exp)
+  (if (not exp)
+    (error 'assert "expression is not true")
+    exp)
+  )
+
+(define (print . args)
+  (define (iter args)
+    (if (not (null? args))
+      (begin
+        (display (car args))(display " ")
+        (iter (cdr args))
+        )))
+  (iter args))
+
+(define (println . args)
+  (print args)(newline)
+  )
