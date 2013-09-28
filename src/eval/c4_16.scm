36c8e2dbda0fb07731af3d90f5570b8977617bb2
seckcoder <seckcoder@gmail.com>
2013-09-28 23:22:50 +0800

    c4 16

---
+++ src/eval/c4_16.scm
@@ -0,0 +1,7 @@
+; a) in eval.scm
+; b) in eval.scm
+; c) if put in make-procedure, than scan-out-defines
+; is evaluated when procedure defined. But when put
+; in procedure-body, scan-out-defines is evaluated when
+; procedure evaluated. It's just a matter of whether
+; we want the internal definition to be lazy.
