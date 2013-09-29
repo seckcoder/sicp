5ea25f0f0f7f744ce383a67a0b0518f495181e2c
seckcoder <seckcoder@gmail.com>
2013-09-29 20:10:34 +0800

    c4 23

---
+++ src/eval/c4_23.scm
@@ -0,0 +1,5 @@
+; I've implemented one analyze-sequence in eval1.scm,
+; the text has a version of analyze-sequence(analyze-sequence-text in eval1.scm)
+; and analyze-sequence-alyssa has a version of analyze-sequence(analyze-sequence-alyssa).
+; Of those, analyze-sequence-text and my version are better. Since they try to expand all the sequence
+; during the analysis stage(try to add a print func in loop).
